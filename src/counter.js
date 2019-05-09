ed57aafc94e647b63e1904a2f47f4289029350c5
Paula Bonastre <bonastrep@gmail.com>
2019-05-09 13:24:32 +0200

    added redux lib

--- src/counter.js
+++ src/counter.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 export const increaseCount = function (amount=1){
     return {
         type: 'INCREASE_COUNT',
@@ -14,8 +16,11 @@ export function reducer (state=DEFAULT_STATE, action={}){
     switch(action.type){
         case 'INCREASE_COUNT':
             return{
+                ...state, // this will copy all properties previous state
                 count: state.count + action.payload
                 }
     }
     return state;
 };
+
+export const store = createStore(reducer);
--- src/counter.spec.js
+++ src/counter.spec.js
@@ -1,6 +1,11 @@
-import {increaseCount, reducer} from "./counter";
+import {increaseCount, reducer, store} from "./counter";
 
 describe('reducer', () => {
+    it('it should return a state even when no state is provided', () =>{
+        const result=reducer();
+        expect(result.count).not.toBeUndefined();
+    })
+
     it('should return a state', () => {
         const result=reducer({count:0});
         expect(result.count).not.toBeUndefined();
@@ -9,5 +14,14 @@ describe('reducer', () => {
     it('should add amount', () => {
         const result=reducer({count:1}, increaseCount(2));
         expect(result.count).toBe(3);
+    });
+
+
+    //Added when we use redux, adding it with npm redux and then importing and exporting is counter.
+    describe ('store',() =>{
+        it('should return the default state', () => {
+            const state = store.getState();
+            expect(state.count).toEqual(0);
+        });
     });
 });
